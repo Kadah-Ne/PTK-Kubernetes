0e113d70790e3b5b5edb430ed75d186433373771
Kadah Ne <mgouviaux@gmail.com>
2025-04-24 10:38:11 +0200

    Changed to MariaDB

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,13 +92,23 @@ CORS_URLS_REGEX = r"^/api/.*$"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "RW",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
